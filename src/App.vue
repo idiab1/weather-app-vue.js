6645b67becb738841b2063e99b18cebf6a7e1ce7
islamdiab-stack <islamdiab967@gmail.com>
2021-05-20 12:46:46 +0200

    Create Home page layout where i created three sections for search box, location section, and weather box for result if warm or rain

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <template>
   <div id="app" class="app">
     <main>
+      <!-- Search Box -->
       <div class="search-box">
         <input class="search-bar" type="text" placeholder="Search..." />
       </div>
+      <!-- End of Search Box -->
+
+      <!-- Weather wrap -->
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">Cairo, Egypt</div>
+          <div class="date">Monday 20 January 2020</div>
+        </div>
+        <div class="weather-box">
+          <div class="temp">
+            <h1>9°C</h1>
+          </div>
+          <div class="weather">
+            <span>Rain</span>
+          </div>
+        </div>
+      </div>
+
+      <!-- End of Weather wrap -->
     </main>
   </div>
 </template>
@@ -14,6 +34,9 @@ export default {
   data() {
     return {
       api_key: "62772885f679b3cedad02112c05144cb",
+      url_base: "https://api.openweathermap.org/data/2.5/",
+      query: "",
+      weather: {},
     };
   },
 };
@@ -43,7 +66,7 @@ main {
   background-image: linear-gradient(
     to bottom,
     rgba(0, 0, 0, 0.25),
-    rgba(0, 0, 0, 0.75)
+    rgb(0 0 0 / 55%)
   );
 }
 
@@ -53,7 +76,7 @@ main {
 }
 .search-box .search-bar {
   display: block;
-  width: 80%;
+  width: 70%;
   margin: auto;
   padding: 13px 10px;
   font-size: 18px;
@@ -68,4 +91,48 @@ main {
 .search-box .search-bar:focus {
   box-shadow: 0 0 16px 1px #3a3939;
 }
+
+/* Weather Box */
+
+.weather-wrap {
+  text-align: center;
+}
+.weather-wrap .location-box .location {
+  font-size: 30px;
+  font-weight: 500;
+  color: #fff;
+  text-shadow: 1px 3px rgb(0 0 0 / 25%);
+}
+.weather-wrap .location-box .date {
+  font-weight: 300;
+  color: #c9c9c9;
+  font-style: italic;
+  font-size: 18px;
+  margin: 5px 0;
+}
+
+.weather-wrap .weather-box .temp {
+  display: inline-block;
+  color: #fff;
+  padding: 20px 50px;
+  background-color: rgba(255, 255, 255, 0.25);
+  box-shadow: 3px 6px rgb(0 0 0 / 25%);
+  margin: 35px 0;
+  border-radius: 14px;
+}
+
+.weather-wrap .weather-box .temp h1 {
+  font-weight: 900;
+  font-size: 34px;
+  text-shadow: 3px 6px rgb(0 0 0 / 25%);
+}
+.weather-wrap .weather-box .weather span {
+  color: #fff;
+  font-size: 21px;
+  font-style: italic;
+  font-weight: 700;
+  text-shadow: 3px 6px rgb(0 0 0 / 25%);
+}
+
+/* End of Weather Box */
 </style>
